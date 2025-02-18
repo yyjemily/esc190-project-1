71237778d8c50a62ff74815f50d6d939b93db5a6
yyjemily <yeyanjunemily@gmail.com>
2025-02-18 16:32:14 -0500

    modified lowest_match function

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "autocomplete.h"
+#include <stdbool.h>
 
 int main(void)
 {
@@ -16,66 +16,72 @@ int main(void)
     return 0;
 }
 
-void read_in_terms(term **terms, int *pnterms, char *filename) {
-    term *terms = (term *)malloc(sizeof(term) * (pnterms));
+void read_in_terms(term *terms, int *pnterms, char filename) {
+    termterms = (term *)malloc(sizeof(term) (pnterms));
 
-    FILE *fp = fopen(filename, "r");
+    FILE fp = fopen(filename, "r");
 
     for (int i = 0; i < pnterms; i++) {
-        fgets(*(terms + i), 200, fp);
+        fgets((terms + i), 200, fp);
     }
 
     // sorting by lex algo:
 
     int sorted = 0 // false
-    char *temp;
+    char temp;
 
     while (sorted == 0) {
         sorted = 1;
         for (int i = 0; i < pnterms - 1; i++) {
-            if (*(terms + i) > *(terms + i + 1)) {
+            if ((terms + i) > (terms + i + 1)) {
                 sorted = 0;
-                temp = *(terms + i);
-                *(terms + i) = *(terms + i + 1);
+                temp =(terms + i);
+                (terms + i) =(terms + i + 1);
                 *(terms + i + 1) = temp;
             }
         }
     }
 
     fclose(fp);
-
-    // sort 
-
-    // The function reads in all the terms from filename,
-    // and places them in the block pointed to by *terms.
-    // The terms should be sorted in lexicographic order
-
-    // fgets(myString, 200, fptr);
-
-    // term t1 = {term, weight};
-    // term *p_t1 = &t1;
-    // term terms[500];
-    // term *terms_block = (term *)malloc(sizeof(term)*500)
-
-    // recall
-        // typedef struct term{
-        //     char term[200]; // assume terms are not longer than 200
-        //     double weight;
-        // } term;
-
-    // read in entire file of terms
-    // term *terms = (term *)malloc(sizeof(term) * (pnterms));
-    // for(int i = 0; i < pnterms; i++) {
-    //     terms[i].name = (char *)malloc(2000 * sizeof(term));
-    // }
-
-    // read in terms from file based on lab exercises
-
-    // to sort alphabetically, "sorted in c"? qsort...? inspired by internet
 }
-
 int lowest_match(term *terms, int nterms, char *substr) {
     // binary search
+
+    // lowest match of the index in terms of the first term in lexicographic ordering that matches the string substr 
+    // while((str[i] >= '0' && str[i] <= '9') || str[i] == '.' ){
+    //check if the first 
+    int left = 0; 
+    int right = nterms-1; 
+    int mid = (left + right)/2;
+
+    int i = 0; //indexs through substr 
+
+    do {
+        mid = (left + right)/2;
+        bool term_match = true; 
+        //if first letter matches 
+        if (terms[mid].term == substr[i]){
+            //while substr not at null and the
+            while((substr[i] != '/0')){
+                if ((terms[mid].term) + 1 == substr[i]){
+                                        
+                }
+                i ++;
+            }
+
+        }
+
+    }while(left <= right);
+
+    
+    while((substr[i] != '/0')){ 
+
+
+    }
+
+    
+    
+    
 }
 
 int highest_match(struct term *terms, int nterms, char *substr) {
