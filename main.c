ad2ecdcfdcd4757e79a773988a242942d23f5dc4
Lia-MC <lia.mc73700@gmail.com>
2025-02-18 12:05:58 -0500

    pseudocode for all functions

--- main.c
+++ main.c
@@ -16,24 +16,28 @@ int main(void)
 }
 
 void read_in_terms(term **terms, int *pnterms, char *filename) {
+    // read in entire file of terms
     // term *terms = (term *)malloc(sizeof(term) * (pnterms));
     // for(int i = 0; i < pnterms; i++) {
     //     terms[i].name = (char *)malloc(2000 * sizeof(term));
     // }
+
+    // read in terms from file based on lab exercises
+
+    // to sort alphabetically, "sorted in c"? qsort...? inspired by internet
 }
 
 int lowest_match(term *terms, int nterms, char *substr) {
-    //
+    // binary search
 }
 
 int highest_match(struct term *terms, int nterms, char *substr) {
-    //
+    // binary search
 }
 
 void autocomplete(term **answer, int *n_answer, term *terms, int nterms, char *substr) {
-    //
+    // cry because using qsort is gonna make us cry
+    // extract terms that match query string
+    // sort by weight -> qsort (# of appearances or by population)
+    // extract top matching terms by weight / output things in order by weight
 }
-
-// search through terms sorted in alphabetical order (binary search)
-// resort things by weight (# of appearances or by population)
-// output things in order by weight
