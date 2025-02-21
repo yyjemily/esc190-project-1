60951e8db5facf923793cad2e1d767c49071994e
Lia-MC <lia.mc73700@gmail.com>
2025-02-21 11:36:54 -0500

    fixed testing highest

--- testinghighest.c
+++ testinghighest.c
@@ -1,59 +1,60 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+// #include <stdbool.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
 
-int main() {
-  struct term *terms;
-  int nterms 5;
+// int main() {
+//   struct term *terms;
+//   int nterms 5;
   
-  create_te
+//   create_te
   
-  terms
+//   terms
   
-  highest_match(terms, nterms, "Tor");
+//   highest_match(terms, nterms, "Tor");
   
 
-  return 0;
-}
+//   return 0;
+// }
 
-void create_term(struct term1 **p_p_t1, const char *term, double weight) {
-    *p_p_t1 = (struct term1*) malloc(sizeof(struct term1)); // THIS *p_p_s1
-    strcpy((*p_p_t1)->term, term);
-    (*p_p_t1)->weight = weight;
-}
+// void create_term(struct term1 **p_p_t1, const char *term, double weight) {
+//     *p_p_t1 = (struct term1*) malloc(sizeof(struct term1)); // THIS *p_p_s1
+//     strcpy((*p_p_t1)->term, term);
+//     (*p_p_t1)->weight = weight;
+// }
 
-void create_terms2_block(struct term2 **p_p_t2, int n_terms) {
-    *p_p_t2 = (struct term2*)malloc(sizeof(struct term2) * n_terms);
-    for(int i = 0; i < n_terms; i++) {
-        (*p_p_t2)[i].name = (char *)malloc(sizeof(char) * 200);
-        ((*p_p_t2)[i])->weight = (double *)malloc(sizeof(double) * 200);
-    }
-}
+// void create_terms2_block(struct term2 **p_p_t2, int n_terms) {
+//     *p_p_t2 = (struct term2*)malloc(sizeof(struct term2) * n_terms);
+//     for(int i = 0; i < n_terms; i++) {
+//         (*p_p_t2)[i].name = (char *)malloc(sizeof(char) * 200);
+//         ((*p_p_t2)[i])->weight = (double *)malloc(sizeof(double) * 200);
+//     }
+// }
 
 typedef struct term{
     char term[200]; // assume terms are not longer than 200
     double weight;
 } term;
 
-int comp(const void *a, const void *b, int len_substr) {
-    term *term_a = (term *)a;
-    term *term_b = (term *)b;
-    return strcmp(term_a->term, term_b->term, len_substr);
-}
+// int comp(const void *a, const void *b, int len_substr) {
+//     term *term_a = (term *)a;
+//     term *term_b = (term *)b;
+//     return strcmp(term_a->term, term_b->term, len_substr);
+// }
 
 int highest_match(struct term *terms, int nterms, char *substr) {  
   // binary search
     int low = 0;
     int high = nterms - 1;
     int mid; 
+    int val;
 
     int len_substr = length(substr);
 
     while (low <= high) {
         // printf("current vals: low: %d, mid: %d, high %d", low, mid, high);
         mid = (low + high) / 2;
-        val = (terms)[mid].term, substr, len_substr;
+        val = comp((terms)[mid].term, substr, len_substr);
         if (val > 0) {
             high = mid - 1;
         } else if (val <= 0) {
